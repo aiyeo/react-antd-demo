398ae100092a239efedcb4b2a41ad10e6bc03e0a
aiyeo <18008092347@163.com>
2017-06-26 18:08:29 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Button,Tree,DatePicker ,Carousel,Modal,Form ,Input} from 'antd';
+import './App.css';
+
+const TreeNode = Tree.TreeNode;
+const { MonthPicker, RangePicker } = DatePicker;
+
+class App extends Component {
+  state = { visible: false }
+  onSelect = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  }
+  onCheck = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  }
+  onChange = (date, dateString) => {
+    console.log(date)
+    console.log(dateString)
+  }
+  afterChange = (a) => {
+    ++a;
+    console.log('第'+a+'张')
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button type="primary">Button</Button>
+      <Tree
+        checkable
+        defaultExpandedKeys={['0-0-0', '0-0-1']}
+        defaultSelectedKeys={['0-0-0', '0-0-1']}
+        defaultCheckedKeys={['0-0-0', '0-0-1']}
+        onSelect={this.onSelect}
+        onCheck={this.onCheck}>
+        <TreeNode title="parent 1" key="0-0">
+          <TreeNode title="parent" key="0-0-0" disabled>
+            <TreeNode title="leaf" key="0-0-0-0" disableCheckbox />
+            <TreeNode title="leaf11" key="0-0-0-1" />
+          </TreeNode>
+          <TreeNode title="parent 1-1" key="0-0-1">
+            <TreeNode title={<span style={{ color: 'red',fontSize:'18px' }}>sss</span>} key="0-0-1-0" />
+          </TreeNode>
+        </TreeNode>
+      </Tree>
+      <RangePicker onChange={this.onChange} />
+      <Carousel afterChange={this.afterChange}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+      </Carousel>
+      <Button type="primary" onClick={this.showModal}>Open</Button>
+      <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <Input type="text" defaultValue="deng" placeholder="test"/>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
